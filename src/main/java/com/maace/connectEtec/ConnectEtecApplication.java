a66d6dd7ac34d81dc225b2c32a3859c3d1c92d0f
Carlos-DMS <carlos.dsanguino@gmail.com>
2024-04-09 16:10:03 -0300

    Projeto criado

---
+++ src/main/java/com/maace/connectEtec/ConnectEtecApplication.java
@@ -0,0 +1,13 @@
+package com.maace.connectEtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectEtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectEtecApplication.class, args);
+	}
+
+}
